facdb8a14afe32057b12914c731a9905c3694314
Dylan Tran <dylanbt928@gmail.com>
2025-05-10 02:23:42 -0700

    added file importing in settings
    Co-authored-by: ASPCoding <ASPCoding@users.noreply.github.com>

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log();
+};
+
+function handleOnChange(e: React.FormEvent<HTMLInputElement>) {
+  const target = e.target as HTMLInputElement & {
+    files: FileList;
+  };
+  console.log("target", target.files);
+}
+
 const Settings: React.FC = () => {
   return (
     <div className="tab-content">
       Settings Details go here
-      <input type="file" name="image" />
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="image" onChange={handleOnChange} />
+        <button type="submit"> Submit </button>
+      </form>
     </div>
   );
 };
