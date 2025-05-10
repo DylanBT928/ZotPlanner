7431be6734f7704078a39a320f93a3e67977451a
Dylan Tran <dylanbt928@gmail.com>
2025-05-09 20:43:53 -0700

    make calendar scalable

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,25 @@ function App() {
       <WeekCalendar />
       <div className="main-content">
         <div>
-          <a href="https://vite.dev" target="_blank"><img src={appLogo} className="logo" alt="Vite logo" /></a>
-          <a href="https://react.dev" target="_blank"><img src={reactLogo} className="logo react" alt="React logo" /></a>
+          <a href="https://vite.dev" target="_blank">
+            <img src={appLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
         </div>
         <h1>Vite + React</h1>
         <div className="card">
-          <button onClick={() => setCount(c => c + 1)}>count is {count}</button>
-          <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
+          <button onClick={() => setCount((c) => c + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
         </div>
-        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
       </div>
     </div>
   );
